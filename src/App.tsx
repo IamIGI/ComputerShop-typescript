bf5653b8ec703d02eab450fba26388ad0ecf63a6
i.kłusek <J6ZkqwVUn>
2022-12-18 20:32:52 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Test from 'components/Test';
+
+function App() {
+    return (
+        <>
+            <h1>App</h1>
+            <Test />
+        </>
+    );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'; // vitejs.dev/config
+
+export default defineConfig({ plugins: [react(), tsconfigPaths()] });
