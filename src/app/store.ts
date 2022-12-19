e31e6df485075be50c22b7417bca9218942c2bc6
i.kłusek <J6ZkqwVUn>
2022-12-20 00:21:01 +0100

    migrate redux files to ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,7 +3,7 @@ import articlesReducer from 'features/articles/articlesSlice';
 import productsReducer from 'features/products/productsSlice';
 import basketReducer from 'features/basket/basketSlice';
 import commentsReducer from 'features/comments/commentsSlice';
-// import { listenerMiddleware } from './middleware';
+import { listenerMiddleware } from './middleware';
 
 export const store = configureStore({
     reducer: {
@@ -12,7 +12,7 @@ export const store = configureStore({
         basket: basketReducer,
         comments: commentsReducer,
     },
-    // middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), listenerMiddleware.middleware],
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), listenerMiddleware.middleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
