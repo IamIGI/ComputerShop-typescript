ff09b2b8674bdeb4079bec20d2535225bd87d94d
i.kłusek <J6ZkqwVUn>
2022-12-26 10:44:08 +0100

    Migrate root files to typescript

---
+++ src/views/Root.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
