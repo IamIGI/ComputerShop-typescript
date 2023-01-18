d125c05b8369d9570aa0ca84b000fdad72adca76
i.kłusek <J6ZkqwVUn>
2023-01-18 16:25:08 +0100

    General Fixes for parch 0.99

--- src/interfaces/product.interfaces.ts
+++ src/interfaces/product.interfaces.ts
@@ -96,6 +96,7 @@ export interface FilterInitInterface {
 }
 
 export interface InitialStateInterface {
+    dataForHomePage: ProductDataInterface[];
     data: ProductDataInterface[];
     status: RequestState;
     error: null | string;
